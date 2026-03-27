ee40e23ce46779212b86a16a6b1d5ead2ee29fec
Ayush Kumar Sahu <www.sahuayush54321@gmail.com>
2026-03-27 22:00:03 +0530

    Refactor API call in LiveSite component

--- client/src/pages/LiveSite.jsx
+++ client/src/pages/LiveSite.jsx
@@ -11,8 +11,7 @@ function LiveSite() {
     const handleGetWebsite = async () => {
       try {
         const result = await axios.get(
-          `${serverUrl}/api/website/get-by-slug/${id}`,
-          { withCredentials: true });
+          `${serverUrl}/api/website/get-by-slug/${id}`);
         setHtml(result.data.latestCode);
         // console.log(result);
       } catch (error) {
